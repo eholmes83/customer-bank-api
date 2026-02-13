2cd33730f1a6c61d08adbc6fbe9888175dbb2e9c
eholmes83 <eholmes83@yahoo.com>
2026-02-12 23:44:44 -0500

    initial commit

---
+++ src/test/java/com/echapps/customerbankapi/CustomerBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echapps.customerbankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerBankApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
