2c98bc4b44cb4532193323ee1b043c1f320d1e90
eholmes83 <eholmes83@yahoo.com>
2026-02-15 19:54:39 -0500

    added/used lombok dependency

--- src/main/java/com/echapps/customerbankapi/customer/model/Customer.java
+++ src/main/java/com/echapps/customerbankapi/customer/model/Customer.java
@@ -1,7 +1,11 @@
 package com.echapps.customerbankapi.customer.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "customers")
 public class Customer {
 
@@ -15,54 +19,4 @@ public class Customer {
     private String email;
     private String phoneNumber;
 
-    public Customer() {
-    }
-
-    public Customer(Long id, String firstName, String lastName, String email, String phoneNumber) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
